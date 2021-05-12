cd3bc9c371da4ba96b7a7d741c56d78db9fe6442
Dan Borza <dan.borza@gmail.com>
2021-05-12 14:53:59 -0700

    Adding APis to inc/get a counter

--- web-server/server.go
+++ web-server/server.go
@@ -5,6 +5,7 @@ import (
     "html"
     "log"
     "net/http"
+    "sync/atomic"
 )
 
 func main() {
@@ -15,6 +16,17 @@ func main() {
 
     http.HandleFunc("/hi", func(w http.ResponseWriter, r *http.Request){
         fmt.Fprintf(w, "Hi")
+    })
+
+    var ops uint64
+
+    http.HandleFunc("/inc", func(w http.ResponseWriter, r *http.Request){
+        atomic.AddUint64(&ops, 1)
+        fmt.Fprintf(w, "Count: %d", ops)
+    })
+
+    http.HandleFunc("/get", func(w http.ResponseWriter, r *http.Request){
+        fmt.Fprintf(w, "Count: %d", ops)
     })
 
     log.Fatal(http.ListenAndServe(":8081", nil))
